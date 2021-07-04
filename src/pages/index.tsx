aa1357f698faae4bc252932228f91e02ae32b30a
Ryan Theodore The <ryan.the.2006@gmail.com>
2021-07-04 23:38:10 +0800

    Add bible verses

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>em-ryan</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Link href="https://github.com/theboi/em-ryanthe.com">View on GitHub</Link>
+      <h3>Available paths:</h3>
+      <ul>
+        <li>cd: Countdown Timer</li>
+        <li>bv: Bible</li>
+      </ul>
     </div>
-  )
+  );
 }
